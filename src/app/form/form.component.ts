faec8458ceb9f8a6923d368fcebcf4030f49aba7
Demolution66 <tonytonychopss@gmail.com>
2019-02-18 01:44:59 +0700

    exam3

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { User } from '../user';
 
 @Component({
@@ -10,6 +10,9 @@ import { User } from '../user';
 export class FormComponent implements OnInit {
   //formBuild:FormBuilder; "//สร้างตัวแปรมาเก็บแบบแรก"
   formGroup: FormGroup;
+  @OutPut() change = new EventEmitter();
+
+
   constructor(
     //fb: FormBuilder "//ใช้ได้แค่ใน constructer เท่านั้นเอาออกมาไม่ได้"
     private formBuild: FormBuilder //เป็นการ auto variable
@@ -18,20 +21,48 @@ export class FormComponent implements OnInit {
   }  //สำหรับ initial ค่าเริ่มต้น
   ngOnInit() {
     this.formGroup =  this.formBuild.group({ //formBuild สร้าง Form
-      firstName: this.formBuild.control(''), //แบบเต็ม
+      firstName: this.formBuild.control('',[Validators.required, Validators.minLength(3)]), //แบบเต็ม
       //firstName: [''],   //สร้าง FormControl แบบย่อ
-      lastName: [''],
-      email: [''],
-      age: ['']
+      lastName: ['',[Validators.required, Validators.minLength(3)]],
+      email: ['',[Validators.email]],
+      age: [,[Validators.min(0),Validators.max(99)]]
       //lastName: ['sdadsadasd'] การ set ค่า sdadsadasd เข้าไป
     })
   }
+  EmilValidator(control: AbstractControl){
+    const value = control.value;
+    if (value && value.includes('@')){
+      return null;
+    }
+    return {
+      email: {
+        acturl: value,
+        expect: 'email@example'
+      }
+    }
+  }
   onSubmit(form: FormGroup){
-    //console.log(form);
-    const {firstName, lastName, email, age} = form.value;
-    const user = new User(firstName, lastName, email , age);
-    console.log(user);
-    //console.log(firstName, lastName);
+    console.log(form.valid, form.invalid);
+    console.log(<FormControl>form.get('firstName').errors);
     
+    if (form.valid) {
+      const {firstName, lastName, email, age} = form.value;
+      const user = new User(firstName, lastName, email , age);
+      console.log(user);
+      this.change.emit()
+    } else {
+      ['firstName', 'lastName', 'age', 'email' ].forEach((key: string) => 
+      {console.log(form.get(key)),
+        form.get(key).markAsTouched()  //กด submit แล้วให้โชว์ required ถ้ายังไม่ได้กรอก
+      
+      }) 
+    }
+    //const {firstName, lastName, email, age} = form.value;
+    // if (!(firstName && firstName.length >= 3)){   "//การ validationแบบทั่วๆไป"
+    //   alert('hello')
+    // }
+    //const user = new User(firstName, lastName, email , age);
+    //console.log(user);
+    //console.log(firstName, lastName);
   }
 }
