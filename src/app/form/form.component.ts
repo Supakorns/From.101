dc0cd2ba77e974ee3c4c612f295221aaa5009e85
Demolution66 <tonytonychopss@gmail.com>
2019-02-17 11:24:38 +0700

    exam3

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
-
+  //formBuild:FormBuilder; "//สร้างตัวแปรมาเก็บแบบแรก"
+  formGroup: FormGroup;
+  constructor(
+    //fb: FormBuilder "//ใช้ได้แค่ใน constructer เท่านั้นเอาออกมาไม่ได้"
+    private formBuild: FormBuilder //เป็นการ auto variable
+  ) { 
+  //  this.formBuild = fb;  "//นำ formBuild มาเก็บค่าของ fb ใน constructer" 
+  }  //สำหรับ initial ค่าเริ่มต้น
   ngOnInit() {
+    this.formGroup =  this.formBuild.group({ //formBuild สร้าง Form
+      firstName: this.formBuild.control(''), //แบบเต็ม
+      //firstName: [''],   //สร้าง FormControl แบบย่อ
+      lastName: ['']
+    })
   }
 
 }
