cfc3b8f98b2a0dc681b907705555a19a607a13b5
Demolution66 <tonytonychopss@gmail.com>
2019-02-17 10:38:59 +0700

    example3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
-  declarations: [
+  declarations: [    //เป็นการประกาศว่าเราจะทำการ new object ขึ้นมา , สร้าง component ขึ้นมาประกาศที่นี่
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule   //
   ],
   providers: [],
   bootstrap: [AppComponent]
